27f9d89cf18f89d5bd8e2909b9129a00e47927e8
tahamurtaza21 <60212648+tahamurtaza21@users.noreply.github.com>
2022-01-20 15:33:13 +0500

    Added Colliders and Triggers

--- Delivery Driver/Assets/Driver.cs
+++ Delivery Driver/Assets/Driver.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+
+    [SerializeField] float steerSpeed = 1f;
+    [SerializeField] float moveSpeed = 0.01f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,8 +16,10 @@ public class Driver : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-         transform.Rotate(0,0,1f);
-         transform.Translate(0,0.4f,0f);
+    {   
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
     }
 }
